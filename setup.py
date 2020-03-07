43ad5891f692fac0acb6361141dd3e4ceab7c891
tlcaputi <tcaputi@gmail.com>
2020-03-07 20:44:41 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='theo_timeline',
     version='1.0.0',
     description='Private package for pulling and merging Google Trends data',
-    url='git@github.com:tlcaputi/timeline.git',
+    url='https://github.com/tlcaputi/timeline.git',
+    packages=find_packages(),
     author='Theodore L Caputi',
     author_email='tcaputi@mit.edu',
     license='No License',
